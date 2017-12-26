f7d14ee73269df839370046e47bd2070914d620c
Jiulong Wang <jiulongw@gmail.com>
2017-12-25 19:27:20 -0800

    Sync XcodePostBuild.cs in project root

--- XcodePostBuild.cs
+++ XcodePostBuild.cs
@@ -80,6 +80,12 @@ public static class XcodePostBuild
 
     private const string BackupExtension = ".bak";
 
+    /// <summary>
+    /// The identifier added to touched file to avoid double edits when building to existing directory without
+    /// replace existing content.
+    /// </summary>
+    private const string TouchedMarker = "https://github.com/jiulongw/swift-unity#v1";
+
     [PostProcessBuild]
     public static void OnPostBuild(BuildTarget target, string pathToBuiltProject)
     {
@@ -253,6 +259,11 @@ public static class XcodePostBuild
         {
             EditMetalHelperMM(Path.Combine(pathToBuiltProject, "Classes/Unity/MetalHelper.mm"));
         }
+
+        if (Application.unityVersion == "2017.3.0f3")
+        {
+            EditSplashScreenMM(Path.Combine(pathToBuiltProject, "Classes/UI/SplashScreen.mm"));
+        }
     }
 
     /// <summary>
@@ -277,12 +288,15 @@ public static class XcodePostBuild
     private static void EditUnityAppControllerH(string path)
     {
         var inScope = false;
+        var markerDetected = false;
+        var markerAdded = false;
 
         EditCodeFile(path, line =>
         {
+            markerDetected |= line.Contains(TouchedMarker);
             inScope |= line.Contains("inline UnityAppController");
 
-            if (inScope)
+            if (inScope && !markerDetected)
             {
                 if (line.Trim() == "}")
                 {
@@ -301,6 +315,16 @@ public static class XcodePostBuild
                     };
                 }
 
+                if (!markerAdded)
+                {
+                    markerAdded = true;
+                    return new string[]
+                    {
+                        "// Modified by " + TouchedMarker,
+                        "// " + line,
+                    };
+                }
+
                 return new string[] { "// " + line };
             }
 
@@ -314,21 +338,31 @@ public static class XcodePostBuild
     private static void EditUnityAppControllerMM(string path)
     {
         var inScope = false;
+        var markerDetected = false;
 
         EditCodeFile(path, line =>
         {
             inScope |= line.Contains("- (void)startUnity:");
+            markerDetected |= inScope && line.Contains(TouchedMarker);
 
             if (inScope && line.Trim() == "}")
             {
                 inScope = false;
 
-                return new string[]
+                if (markerDetected)
                 {
-                    "    // Post a notification so that Swift can load unity view once started.",
-                    @"    [[NSNotificationCenter defaultCenter] postNotificationName: @""UnityReady"" object:self];",
-                    "}",
-                };
+                    return new string[] { line };
+                }
+                else
+                {
+                    return new string[]
+                    {
+                        "    // Modified by " + TouchedMarker,
+                        "    // Post a notification so that Swift can load unity view once started.",
+                        @"    [[NSNotificationCenter defaultCenter] postNotificationName: @""UnityReady"" object:self];",
+                        "}",
+                    };
+                }
             }
 
             return new string[] { line };
@@ -340,19 +374,79 @@ public static class XcodePostBuild
     /// </summary>
     private static void EditMetalHelperMM(string path)
     {
+        var markerDetected = false;
+
         EditCodeFile(path, line =>
         {
-            if (line.Trim() == "surface->stencilRB = [surface->device newTextureWithDescriptor: stencilTexDesc];")
+            markerDetected |= line.Contains(TouchedMarker);
+
+            if (!markerDetected && line.Trim() == "surface->stencilRB = [surface->device newTextureWithDescriptor: stencilTexDesc];")
             {
                 return new string[]
                 {
                     "",
+                    "    // Modified by " + TouchedMarker,
                     "    // Default stencilTexDesc.usage has flag 1. In runtime it will cause assertion failure:",
                     "    // validateRenderPassDescriptor:589: failed assertion `Texture at stencilAttachment has usage (0x01) which doesn't specify MTLTextureUsageRenderTarget (0x04)'",
                     "    // Adding MTLTextureUsageRenderTarget seems to fix this issue.",
                     "    stencilTexDesc.usage |= MTLTextureUsageRenderTarget;",
                     line,
                 };
+            }
+
+            return new string[] { line };
+        });
+    }
+
+    /// <summary>
+    /// Edit 'SplashScreen.mm': Unity introduces its own 'LaunchScreen.storyboard' since 2017.3.0f3.
+    /// Disable it here and use Swift project's launch screen instead.
+    /// </summary>
+    private static void EditSplashScreenMM(string path) {
+        var markerDetected = false;
+        var markerAdded = false;
+        var inScope = false;
+        var level = 0;
+
+        EditCodeFile(path, line =>
+        {
+            inScope |= line.Trim() == "void ShowSplashScreen(UIWindow* window)";
+            markerDetected |= line.Contains(TouchedMarker);
+
+            if (inScope && !markerDetected)
+            {
+                if (line.Trim() == "{")
+                {
+                    level++;
+                }
+                else if (line.Trim() == "}")
+                {
+                    level--;
+                }
+
+                if (line.Trim() == "}" && level == 0)
+                {
+                    inScope = false;
+                }
+
+                if (level > 0 && line.Trim().StartsWith("bool hasStoryboard"))
+                {
+                    return new string[]
+                    {
+                        "    // " + line,
+                        "    bool hasStoryboard = false;",
+                    };
+                }
+
+                if (!markerAdded)
+                {
+                    markerAdded = true;
+                    return new string[]
+                    {
+                        "// Modified by " + TouchedMarker,
+                        line,
+                    };
+                }
             }
 
             return new string[] { line };
@@ -370,6 +464,11 @@ public static class XcodePostBuild
     private static void EditCodeFile(string path, Func<string, IEnumerable<string>> lineHandler)
     {
         var bakPath = path + ".bak";
+        if (File.Exists(bakPath))
+        {
+            File.Delete(bakPath);
+        }
+
         File.Move(path, bakPath);
 
         using (var reader = File.OpenText(bakPath))
